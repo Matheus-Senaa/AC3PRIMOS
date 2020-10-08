389a2c4177658525e52623a0a08a115cb1a2b0df
Matheus-Senaa <itzsena16@gmail.com>
2020-10-08 19:54:06 -0300

    AC3PRIMOSS

---
+++ primos.py
@@ -0,0 +1,32 @@
+import os
+from flask import Flask, jsonify, request
+from math import sqrt
+
+app = Flask(__name__)
+
+@app.route('/')
+def nao_entre_em_panico():
+
+print('Esses são os 100 primeiros numeros primos: ')
+cont=0;
+numero = 2;
+
+while (cont < 100):
+	primo = False;
+	for i in range (2, numero):
+		if (numero%i == 0):
+			primo = True;
+			break;
+
+	if (not primo):
+		cont += 1;
+		print(numero);
+
+	numero += 1;
+
+
+    return primos
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
